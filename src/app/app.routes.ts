a37a541622dc89cb5e3731652d3ad80480005adf
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-08-17 21:29:09 +0330

    Routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {List} from './_pages/list/list';
+import {Add} from './_pages/add/add';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: List },
+  { path: 'add', component: Add },
+];
